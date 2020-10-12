30a10f858e4c4b9105a9e075c4bc246982124c44
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-12 12:13:53 +0530

    [Saurabh]Added: Invoice generator returning total rides, total fare and average

---
+++ src/main/java/InvoiceSummary.java
@@ -0,0 +1,24 @@
+public class InvoiceSummary {
+
+    private final int numberRides;
+    private double totalFare;
+    private double averageFare;
+
+    public InvoiceSummary(int numberRides, double totalFare) {
+    this.numberRides = numberRides;
+    this.totalFare = totalFare;
+    this.averageFare = this.totalFare/this.numberRides;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary that = (InvoiceSummary) o;
+        return numberRides == that.numberRides &&
+                Double.compare(that.totalFare, totalFare) == 0 &&
+                Double.compare(that.averageFare, averageFare) == 0;
+    }
+
+}
---
+++ src/test/java/InvoiceServiceTest.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+InvoiceGenerator invoiceGenerator = null;
+    @Before
+    public void setUp(){
+        invoiceGenerator = new InvoiceGenerator();
+    }
+    @Test
+    public void givingDistance(){
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance ,time);
+        Assert.assertEquals(25,fare,0.0);
+   }
+    @Test
+    public void givingLessDistance_ShouldReturnFare(){
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance ,time);
+        Assert.assertEquals(5,fare,0.0);
+
+    }
+    @Test
+    public void givingMultipleRides_ShouldReturnInvoiceSummary(){
+        Ride[] rides = {new Ride(2.0,5),
+                        new Ride(0.1,1)};
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+       InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2,30.0);
+        Assert.assertEquals(expectedInvoiceSummary,summary);
+    }
+}
+
